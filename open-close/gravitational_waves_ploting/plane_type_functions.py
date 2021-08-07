aa0091bfa4a77103fe49a77abd6596e486c63d89
Chris-48 <christianmgoes@gmail.com>
2021-08-07 13:29:24 -0300

    I like static typed languages.

--- open-close/gravitational_waves_ploting/plane_type_functions.py
+++ open-close/gravitational_waves_ploting/plane_type_functions.py
@@ -1,9 +1,15 @@
-from types import FunctionType
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 import matplotlib.pyplot as plt
+from types import FunctionType
+from numpy import ndarray
 import numpy as np
 
+STOP_LINSPACE = 24
+NUM_LINSPACE = 100
+FRAME_DISPLAY_INTERVAL = .084
 
-def gravitational_wave_function_of_time(X, Y, T):
+
+def gravitational_wave_function_of_time( X: ndarray, Y: ndarray, T: float ):
     """
     The four dimensional function that represents a gravitational wave in the plane.
 
@@ -13,17 +19,16 @@ def gravitational_wave_function_of_time(X, Y, T):
     return 1/4 * np.cos(1/2 * np.pi * np.sqrt(X**2 + Y**2) - (np.pi*T)/2)
 
 
-def static_plane(ax, X, Y):
-
+def static_plane( ax: Axes3D, X: ndarray, Y: ndarray ) -> None:
     """
     View of the R^3 plane without camera motion.
     """
-    for T in np.linspace(0, 20, 100):
+    for T in np.linspace(0, STOP_LINSPACE, NUM_LINSPACE):
 
         Z = gravitational_wave_function_of_time(X, Y, T)
         wframe = ax.plot_wireframe(X, Y, Z,  rstride=2, cstride=2)
 
-        plt.pause(.07)
+        plt.pause(FRAME_DISPLAY_INTERVAL)
         ax.collections.remove(wframe)
 
 
@@ -33,27 +38,27 @@ def rotation_plane() -> FunctionType:
     """
     azimuth = 0
 
-    def _rotation_plane(ax, X, Y, elevation, start_azimuth) -> None:
+    def _rotation_plane( ax: Axes3D, X: ndarray, Y: ndarray, elevation: float, start_azimuth: float ) -> None:
 
         nonlocal azimuth
 
         azimuth = start_azimuth if azimuth == 0 else azimuth
 
-        for T in np.linspace(0, 24, 100):
+        for T in np.linspace(0, STOP_LINSPACE, NUM_LINSPACE):
 
             azimuth += .2
-            ax.view_init(elev=elevation, azim=azimuth)
+            ax.view_init(elev=elevation, azim=azimuth )
 
             Z = gravitational_wave_function_of_time(X, Y, T)
             wframe = ax.plot_wireframe(X, Y, Z,  rstride=2, cstride=2)
 
-            plt.pause(.084)
+            plt.pause(FRAME_DISPLAY_INTERVAL)
             ax.collections.remove(wframe)
 
     return _rotation_plane
 
 
-def map_wave_fucntion_types(ax, input_space, view_point):
+def map_wave_fucntion_types( ax: Axes3D, input_space: tuple, view_point: list ) -> dict:
     """
     Map all the wave functions and their respective inputs to char keys.
     """
