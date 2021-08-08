93a066a0f93075ffdbb295e61eeae153a4237a0a
Chris-48 <christianmgoes@gmail.com>
2021-08-08 15:32:05 -0300

    Naming variables is the hardest task in CS.

--- open-close/gravitational_wave_ploting/gravitational_wave_function_of_time.py
+++ open-close/gravitational_wave_ploting/gravitational_wave_function_of_time.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-FUNCTION_OUTPUT_SCALE = 1/4
-LAMBDA_INVERSE_SCALE = 1/2
+AMPLITUDE = 1/4
+LAMBDA = 1/2
 
 
 def gravitational_wave_function_of_time( X: np.ndarray, Y: np.ndarray, T: float ):
@@ -11,4 +11,4 @@ def gravitational_wave_function_of_time( X: np.ndarray, Y: np.ndarray, T: float 
     `f(t, x, y) = θ cos(λπ√(x^2 + y^2) - (tπ)/2)`
     """
 
-    return FUNCTION_OUTPUT_SCALE * np.cos(LAMBDA_INVERSE_SCALE * np.pi * np.sqrt(X**2 + Y**2) - (np.pi*T)/2)
+    return AMPLITUDE * np.cos(LAMBDA * np.pi * np.sqrt(X**2 + Y**2) - (np.pi*T)/2)
